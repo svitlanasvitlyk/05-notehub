0fa66cad5ebf44c441f1da9930623aeb21a77348
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2025-12-26 12:20:13 +0100

    update main

--- main.tsx
+++ main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './src/components/App/App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import 'modern-normalize';
+import 'modern-normalize/modern-normalize.css';
 
 const queryClient = new QueryClient();
 
