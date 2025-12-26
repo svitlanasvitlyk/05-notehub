7943257532084a5ecebdf2a8f57fe88a3a77a6d3
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2025-12-26 17:46:01 +0100

    update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_NOTEHUB_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
