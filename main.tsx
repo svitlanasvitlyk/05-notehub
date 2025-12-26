40534f6f634d5db0faf472b136be6b6cf79782d4
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2025-12-26 12:17:22 +0100

    update

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App/App.tsx';
+import App from './src/components/App/App.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import 'modern-normalize';
